5547377df001f27cee1d51f2bd27c6a4982fc5c0
rjEks <rjekstein@gmail.com>
2022-09-07 15:32:20 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-
 # This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
 # Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
-import logging 
+import logging
 import ask_sdk_core.utils as ask_utils
 
 from ask_sdk_core.skill_builder import SkillBuilder
@@ -13,6 +12,7 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
 import json
+import random
 
 from ask_sdk_model import Response
 
@@ -52,12 +52,13 @@ class RequestPhraseHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = json_content["frases"][1]["text"]
+        requested_phrase = random.choice(json_content["frases"])
+        speak_output = requested_phrase["text"]
 
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .ask("Se quiser mais uma frase do mestre Chuck norris, diga qual a frase do dia")
                 .response
         )
 
